e403922b31d857df863cd2af1bf6e70c0af67d43
Choonghyun Ryu <choonghyun.ryu@gmail.com>
2023-07-23 08:38:37 +0900

    add pkgdown docs

---
+++ R/bitTomato.R
@@ -0,0 +1,16 @@
+#' Text data of articles published in News Tomato
+#' @docType package
+#' @description Text data created from news articles provided by the media
+#' company NewsTomato. It includes examples of text analysis using this data,
+#' as well as functions and vignettes.
+#' @details
+#' \tabular{ll}{
+#' Package: \tab bitTomato\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1.0\cr
+#' Date: \tab 2023-07-22\cr
+#' License: \tab GPL-3\cr
+#' }
+#' @author 유충현
+#' Maintainer: 유충현 <choonghyun.ryu@gmail.com>
+"_PACKAGE"
